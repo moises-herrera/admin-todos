1c64214b2da444ff795a540d2dbc8a4dc9871063
moises-herrera <moisesherrera037@gmail.com>
2024-02-26 21:53:05 -0500

    refactor: update environment variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,4 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
   redirect('/dashboard');
-
-  return (
-    <>
-      <span className="text-5xl">Home</span>
-    </>
-  );
 }
